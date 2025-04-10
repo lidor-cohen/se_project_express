aac99e34b881660427e9f928949821cdbbbab98a
Lidor Cohen <lidorcohen.p1@gmail.com>
2025-04-10 20:19:01 +0300

    init folder structure and base files

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const { PORT = 3001 } = process.env;
+const app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+
+app.listen(PORT, () => {
+  console.log(PORT);
+});
